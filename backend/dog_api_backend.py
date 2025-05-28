db484327d984f94ecdbc074451dd16130c11cc2f
Daniel Montes <gdanielmontesh9@gmail.com>
2025-05-27 18:12:47 -0600

    feat: add breeds endpoint

--- backend/dog_api_backend.py
+++ backend/dog_api_backend.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+import urllib.request
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +8,18 @@ app = Flask(__name__)
 @app.route('/hello', methods=['GET'])
 def get_hello():
     return "hello world!"
+
+@app.route('/breeds', methods=['GET'])
+def get_breeds():
+    try:
+        with urllib.request.urlopen('https://dogapi.dog/api/v2/breeds') as response:
+            data = response.read()
+            breeds = json.loads(data)
+
+        return json.dumps(breeds), 200, {'Content-Type': 'application/json'}
+
+    except Exception as e:
+        return json.dumps({'error': str(e)}), 500, {'Content-Type': 'application/json'}
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
