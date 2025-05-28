69c519336323daee41a89e1bb4dddc6c0a88c3eb
Daniel Montes <gdanielmontesh9@gmail.com>
2025-05-27 18:36:30 -0600

    feat: add facts endpoint

--- backend/dog_api_backend.py
+++ backend/dog_api_backend.py
@@ -33,5 +33,18 @@ def get_breed_byID(breed_id):
     except Exception as e:
         return json.dumps({'error': str(e)}), 500, {'Content-Type': 'application/json'}
 
+@app.route('/facts', methods=['GET'])
+def get_facts():
+    try:
+        with urllib.request.urlopen('https://dogapi.dog/api/v2/facts') as response:
+            data = response.read()
+            facts = json.loads(data)
+
+        return json.dumps(facts), 200, {'Content-Type': 'application/json'}
+
+    except Exception as e:
+        return json.dumps({'error': str(e)}), 500, {'Content-Type': 'application/json'}
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
