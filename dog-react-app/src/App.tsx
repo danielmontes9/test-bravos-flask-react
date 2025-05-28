6cf62d2a165f386b727d91a373db22898c6edd7c
Daniel Montes <gdanielmontesh9@gmail.com>
2025-05-27 23:21:31 -0600

    feat: update ui to search data

--- dog-react-app/src/App.tsx
+++ dog-react-app/src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>DOG API APP 🐶</h1>
+      <h2></h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className='filter-options'>
+          <div className='opt'>
+            <input type="radio" id="breeds" name="filter_option" value="Breeds" />
+            <label>Breeds</label>
+          </div>
+          
+
+          <div className='opt'>
+            <input type="radio" id="facts" name="filter_option" value="Facts" />
+            <label>Facts</label>
+          </div>
+          
+
+          <div className='opt'>
+            <input type="radio" id="groups" name="filter_option" value="Groups" />
+            <label>Groups</label>
+          </div>
+
+          <div className='opt'>
+            <input type="radio" id="groups_details" name="filter_option" value="Groups Details" />
+            <label>Groups Details</label>
+          </div>
+        </div>
+
+        <div className='button-container'>
+          <button>
+            Get Data!
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
